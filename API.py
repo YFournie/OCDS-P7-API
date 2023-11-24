4128135884bb97a852710af40fbd425d47e9809a
YFournie <138864726+YFournie@users.noreply.github.com>
2023-11-24 10:12:46 +0100

    Correcting small "reset index" bug

--- API.py
+++ API.py
@@ -14,7 +14,7 @@ app.config["DEBUG"] = True
 
 # On récupère notre jeu de test, avec les identifiants des clients
 clients_raw = pd.read_csv("smaller_test_8.csv")
-clients = clients_raw.sort_values(by = "SK_ID_CURR")
+clients = clients_raw.sort_values(by = "SK_ID_CURR").reset_index()
 
 # On récupère le modèle enregistré
 model = pickle.load(open('selected_model.sav', 'rb'))
@@ -23,7 +23,7 @@ model = pickle.load(open('selected_model.sav', 'rb'))
 def get_client_feats(clients, id):
     """
     Gives us the list of features for our specific client id
-    
+
     Parameters
     ----------
     clients : DataFrame
@@ -41,7 +41,7 @@ def get_client_feats(clients, id):
         raise TypeError("The first argument must be a dataframe")
     elif id not in clients["SK_ID_CURR"].values :
         raise ValueError("this id does not belong to any client we know of")
-    else: 
+    else:
         client = clients.loc[clients["SK_ID_CURR"] == id,:].drop(columns = ["SK_ID_CURR"]).values
         return client
 
@@ -55,19 +55,19 @@ def prob():
         id = int(request.args['id'])
     else:
         return "Error: No id field provided. Please specify an id."
-        
+
     # Now we check if the id is valid (is it in test_8 ?)
     if id not in clients["SK_ID_CURR"].values :
         return "Error : Invalid id provided. id not in testing set"
     else:
         # We select the good row and drop the id
         client = clients.loc[clients["SK_ID_CURR"] == id,:].drop(columns = ["SK_ID_CURR"])
-        
+
         # We get a prediction
         predicted_failure_rate = model.predict_proba(client.values)[0][1]
-        
+
         response = str(predicted_failure_rate)
-    
+
     return response
 
 @app.route('/clients_list', methods = ['GET'])
@@ -85,10 +85,10 @@ def clifeats():
         id = int(request.args['id'])
     else:
         return "Error: No id field provided. Please specify an id."
-        
+
     client = get_client_feats(clients, id)
     prep_client = model[:-1].transform(client)
-        
+
     return prep_client.tolist()
 
 #Pour l'affichage des features, non scalées
@@ -101,9 +101,9 @@ def clientfeats():
         id = int(request.args['id'])
     else:
         return "Error: No id field provided. Please specify an id."
-        
+
     client = get_client_feats(clients, id)
-        
+
     return client.tolist()
 
 #Pour l'affichage des features des clients "similaires"
@@ -114,13 +114,13 @@ def smilarclients():
     data_id = data["id"]
     client_index = clients.index[clients["SK_ID_CURR"] == data_id]
     feature_list = data["features"]
-    
+
     # Using a nearest neighbors tool to get the neighbors
     for_neighbors = clients.loc[:,["DAYS_BIRTH","AMT_INCOME_TOTAL","AMT_CREDIT"]].values
     nn = NearestNeighbors(n_neighbors = 11)
     nn.fit(for_neighbors)
     distance, indices = nn.kneighbors(for_neighbors[client_index].reshape(1,-1),11)
-    
+
     # Building a DataFrame with our wanted features
     df_neighbors = pd.DataFrame(columns = feature_list)
     j = 0
@@ -128,8 +128,8 @@ def smilarclients():
         # Une ligne à rallonge pour cause de formatage...
         df_neighbors.loc[j] = clients.iloc[[i]][feature_list].values.tolist()[0]
         j += 1
-    
+
     return df_neighbors.to_dict()
-    
-    
+
+
 # app.run(debug=True, use_reloader=False)
